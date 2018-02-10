70ebf3554a048d722d55c700692196b2b1f814d3
Rebecca Strauss <rstrauss127@gmail.com>
2018-02-10 21:01:33 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,16 @@
 class EmailParser
 
   attr_accessor :list
+
+  def initialize(list)
+    @list = list
+  end
+
+  def parse
+    if @list.include?(",")
+      @list.split(",")
+    else
+      @list.split(" ")
+    end
+  end
 end
